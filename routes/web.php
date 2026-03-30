74134be291bafebdf896fdef3782f036b837cb08
Gianluca <gporta@gianlucaporta.it>
2026-03-30 12:29:44 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@ Route::get('/carta-intestata', function () {
     return view('print.carta-intestata');
 })->name('print.carta-intestata');
 
-
+/*invoice*/
 Route::get('/invoice/{invoice}/print', [InvoiceController::class, 'print'])->name('invoice.print');
 Route::get('/invoice/{invoice}/xml', [InvoiceXmlController::class, 'generate'])->name('invoice.xml');
+/*invoice*/
+/*quote*/
+Route::get('/quote/{invoice}/print', [InvoiceController::class, 'print'])->name('quote.print');
+Route::get('/quote/{invoice}/xml', [InvoiceXmlController::class, 'generate'])->name('quote.xml');
+/*quote*/
