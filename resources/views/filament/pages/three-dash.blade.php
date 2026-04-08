374c2659ef0c8bd41e24f8c8507cb0b886edb92f
Gianluca <gporta@gianlucaporta.it>
2026-04-08 18:30:28 +0200

    fix

--- resources/views/filament/pages/three-dash.blade.php
+++ resources/views/filament/pages/three-dash.blade.php
@@ -45,14 +45,14 @@
                     </tbody>
                 </table>
                 @if(!empty($package->ore_per_user))
-                <div class="mt-4 text-xs border-t border-gray-800 pt-3">
+                <div class="mt-4 text-xs border-t border-gray-800 pt-3 text-center">
                     <div class="text-gray-400 mb-2">Ore per utente</div>
 
-                    <div class="flex flex-wrap gap-2">
+                    <div class="flex flex-wrap justify-center gap-2">
                         @foreach($package->ore_per_user as $userOre)
                         <span class="px-2 py-1 bg-gray-800 rounded text-gray-200">
-                {{ $userOre }}
-            </span>
+            {{ $userOre }}
+        </span>
                         @endforeach
                     </div>
                 </div>
